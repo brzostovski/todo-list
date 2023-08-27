85147505ced139547ea6fa976833f188305f206b
brzostovski <137798050+brzostovski@users.noreply.github.com>
2023-08-27 14:30:34 +0200

    Initial commit

---
+++ src/notes.js
@@ -0,0 +1,18 @@
+// this is module for notes objects
+
+export default function notes() {
+  console.log('notes');
+};
+
+// GLOBAL OBJECT -> PROJECTS -> NOTES
+// by default new notes will be created in project 'Inbox'
+
+// here will be objects to create global object holding projects
+
+// here will be objects - projects containing notes
+
+// here will be note object that will contain:
+// title
+// description
+// dueDate
+// flag: true/false (instead of priority)
