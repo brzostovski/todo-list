5649938ac7ccd69bf4a9c35e43c374845953cc19
brzostovski <137798050+brzostovski@users.noreply.github.com>
2023-08-29 21:33:00 +0200

    Display passed object (note) in <main>

--- src/render.js
+++ src/render.js
@@ -1,3 +1,19 @@
-export default function render(object) {
-  console.log(object);
-}
+export default (function render(project, domElement) {
+  project.notes.forEach(note => {
+    domElement.appendChild(createNoteCard(note).card);
+  });
+});
+
+let createNoteCard = (function(note) {
+  let card = document.createElement('div');
+  card.classList.add('note');
+
+  Object.keys(note).forEach(key => {
+    let _section = document.createElement('div');
+    _section.classList.add(key);
+    _section.textContent = note[key];
+    card.appendChild(_section);
+  });
+
+  return {card};
+});
