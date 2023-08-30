be7512d766b053edc513e16151eb9642d91e16f1
brzostovski <137798050+brzostovski@users.noreply.github.com>
2023-08-30 17:22:23 +0200

    Generate more test notes

--- src/notes.js
+++ src/notes.js
@@ -28,12 +28,15 @@ export default (function notes() {
   let inbox = new Project('Inbox');
   let defaultNote = new Note(
     'Example Note',
-    'This is an example note!',
+    'This is an example note! And this is just more and more text to see how the notes are being displayed after styling them a bit.',
     '12.12.2024',
     false
   );
 
-  inbox.notes.push(defaultNote);
+  let shortNote = new Note('Short Note', 'This note is shorter!', '', true);
+
+  for(let i = 0; i < 20; i++){inbox.notes.push(defaultNote);};
+  inbox.notes[5] = shortNote;
   projects.push(inbox);
 
   return {projects};
