71d5b0eedcceae8a883c54477bdd244839b26ecb
brzostovski <137798050+brzostovski@users.noreply.github.com>
2023-09-04 19:46:28 +0200

    Console log new note on "Add note" button click

--- src/index.js
+++ src/index.js
@@ -7,16 +7,16 @@ import render from './render';
   let pageContents = initPage().pageContents;
   render(notes().projectsArr[0], pageContents.main);
 
-  const bindEvents = (function() {
-    const form = document.querySelector('form');
-    const addNoteBtn = document.getElementById('add-note-btn');
+  const form = document.querySelector('form');
+  const addNoteBtn = document.getElementById('add-note-btn');
 
+  const bindEvents = (function() {
     addNoteBtn.onclick = function() {
       let isFormValid = form.checkValidity();
       if (!isFormValid) {
         form.reportValidity();
       } else {
-      console.log('click');
+        console.log(notes().createNote());
       }
     };
   })();
