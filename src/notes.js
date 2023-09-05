f38c5055cbc62318345a618917977876b71a15be
brzostovski <137798050+brzostovski@users.noreply.github.com>
2023-09-05 18:01:42 +0200

    General module rework (more in description)
    Function reworked to not run everything every time an object or method
    is called. Added newNote method to use data from form inputs to create
    new note and push it to default project ("Inbox").

--- src/notes.js
+++ src/notes.js
@@ -1,58 +1,49 @@
 export default (function notes() {
-  let projectsArr = [];
-
-  let Project = (function (name, ...notes) {
-    this.name = name;
-    this.notes = notes;
-  
-    return {
-      name,
-      notes,
-    }
-  });
-
-  let inbox = new Project('Inbox');
-  
-  let Note = (function (title, description, dueDate, flag) {
-    this.title = title;
-    this.description = description;
-    this.dueDate = dueDate;
-    this.flag = flag;
-  
-    return {
-      title,
-      description,
-      dueDate,
-      flag,
-    }
-  });
-
-  function createNote() {
-    const _title = document.getElementById('title').value;
-    const _desc = document.getElementById('desc').value;
-    const _date = document.getElementById('date').value;
-    const _flag = document.getElementById('flag-button').checked;
-
-    return (new Note(_title, _desc, _date, _flag));
-  };
-  
-  let defaultNote = new Note(
-    'Example Note',
-    'This is an example note! And this is just more and more text to see how the notes are being displayed after styling them a bit.',
-    '12.12.2024',
-    false
-  );
-
-  let shortNote = new Note('Short Note', 'This note is shorter!', '', true);
-
-  for(let i = 0; i < 20; i++){inbox.notes.push(defaultNote);};
-  inbox.notes[2] = shortNote;
-  projectsArr.push(inbox);
-
   return {
     projectsArr,
     Project,
-    Note,
-    createNote,
+    addNote,
   };
 });
+
+let Project = (function (name, ...notes) {
+  this.name = name;
+  this.notes = notes;
+
+  return {
+    name,
+    notes,
+  }
+});
+
+let projectsArr = [];
+let inbox = new Project('Inbox'); // this creates default project called "Inbox"
+projectsArr[0] = inbox;
+
+let Note = (function (title, description, dueDate, flag) {
+  this.title = title;
+  this.description = description;
+  this.dueDate = dueDate;
+  this.flag = flag;
+
+  return {
+    title,
+    description,
+    dueDate,
+    flag,
+  }
+});
+
+function _createNote() {
+  let _title = document.getElementById('title').value;
+  let _desc = document.getElementById('desc').value;
+  let _date = document.getElementById('date').value;
+  let _flag = document.getElementById('flag-button').checked;
+
+  return (new Note(_title, _desc, _date, _flag));
+};
+
+function addNote() {
+  let _note = _createNote();
+  projectsArr[0].notes.push(_note);
+};
