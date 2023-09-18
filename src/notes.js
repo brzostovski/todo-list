22256083ee36ccc425bfd87a2ae427c86fd0106a
brzostovski <137798050+brzostovski@users.noreply.github.com>
2023-09-18 21:36:42 +0200

    Rewrite notes as object

--- src/notes.js
+++ src/notes.js
@@ -1,58 +1,52 @@
-export default (function notes() {
-  return {
-    projectsArr,
-    Project,
-  };
-});
-
-let Project = (function (name, ...notes) {
-  this.name = name;
-  this.notes = notes;
-
-  const _getNewNoteData = (function() {
-    return {
-      title: document.getElementById('title-input').value,
-      desc: document.getElementById('desc-input').value,
-      date: document.getElementById('date-input').value,
-      flag: document.getElementById('flag-btn').checked,
+const notes = {
+  projectsArr: [],
+  Project: (function (name, ...notes) {
+    this.name = name;
+    this.notes = notes;
+  
+    let _Note = (function (title, description, dueDate, flag) {
+      this.title = title;
+      this.description = description;
+      this.dueDate = dueDate;
+      this.flag = flag;
+    
+      return {
+        title,
+        description,
+        dueDate,
+        flag,
+      }
+    });
+  
+    const _getNewNoteData = (function() {
+      return {
+        title: document.getElementById('title-input').value,
+        desc: document.getElementById('desc-input').value,
+        date: document.getElementById('date-input').value,
+        flag: document.getElementById('flag-btn').checked,
+      };
+    });
+    
+    function _createNote() {
+      let _title = _getNewNoteData().title;
+      let _desc = _getNewNoteData().desc;
+      let _date = _getNewNoteData().date;
+      let _flag = _getNewNoteData().flag;
+    
+      return (new _Note(_title, _desc, _date, _flag));
     };
-  });
   
-  function _createNote() {
-    let _title = _getNewNoteData().title;
-    let _desc = _getNewNoteData().desc;
-    let _date = _getNewNoteData().date;
-    let _flag = _getNewNoteData().flag;
+    function addNote() {
+      let _note = _createNote();
+      notes.push(_note);
+    };
   
-    return (new Note(_title, _desc, _date, _flag));
-  };
+    return {
+      name,
+      notes,
+      addNote,
+    }
+  }),
+};
 
-  function addNote() {
-    let _note = _createNote();
-    notes.push(_note);
-  };
-
-  return {
-    name,
-    notes,
-    addNote,
-  }
-});
-
-let projectsArr = [];
-let inbox = new Project('Inbox'); // this creates default project called "Inbox"
-projectsArr[0] = inbox;
-
-let Note = (function (title, description, dueDate, flag) {
-  this.title = title;
-  this.description = description;
-  this.dueDate = dueDate;
-  this.flag = flag;
-
-  return {
-    title,
-    description,
-    dueDate,
-    flag,
-  }
-});
+export default notes;
