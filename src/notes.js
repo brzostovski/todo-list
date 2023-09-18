b50d961ddf6e85e310f2a778bdd53194026d1468
brzostovski <137798050+brzostovski@users.noreply.github.com>
2023-09-18 21:25:43 +0200

    Move helper functions inside Project

--- src/notes.js
+++ src/notes.js
@@ -1,5 +1,3 @@
-import page from "./page";
-
 export default (function notes() {
   return {
     projectsArr,
@@ -10,6 +8,24 @@ export default (function notes() {
 let Project = (function (name, ...notes) {
   this.name = name;
   this.notes = notes;
+
+  const _getNewNoteData = (function() {
+    return {
+      title: document.getElementById('title-input').value,
+      desc: document.getElementById('desc-input').value,
+      date: document.getElementById('date-input').value,
+      flag: document.getElementById('flag-btn').checked,
+    };
+  });
+  
+  function _createNote() {
+    let _title = _getNewNoteData().title;
+    let _desc = _getNewNoteData().desc;
+    let _date = _getNewNoteData().date;
+    let _flag = _getNewNoteData().flag;
+  
+    return (new Note(_title, _desc, _date, _flag));
+  };
 
   function addNote() {
     let _note = _createNote();
@@ -40,21 +56,3 @@ let Note = (function (title, description, dueDate, flag) {
     flag,
   }
 });
-
-const getNewNoteData = (function() {
-  return {
-    title: document.getElementById('title-input').value,
-    desc: document.getElementById('desc-input').value,
-    date: document.getElementById('date-input').value,
-    flag: document.getElementById('flag-btn').checked,
-  };
-});
-
-function _createNote() {
-  let _title = getNewNoteData().title;
-  let _desc = getNewNoteData().desc;
-  let _date = getNewNoteData().date;
-  let _flag = getNewNoteData().flag;
-
-  return (new Note(_title, _desc, _date, _flag));
-};
