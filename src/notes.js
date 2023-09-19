c152159430c98c0fc4a648872dcfccb44746cf8a
brzostovski <137798050+brzostovski@users.noreply.github.com>
2023-09-19 19:07:43 +0200

    Change project to be Note's property

--- src/notes.js
+++ src/notes.js
@@ -1,32 +1,26 @@
 const notes = {
-  projectsArr: [],
-  Project: (function (name, ...notes) {
-    this.name = name;
-    this.notes = notes;
-  
-    let _Note = (function (title, description, dueDate, flag) {
-      this.title = title;
-      this.description = description;
-      this.dueDate = dueDate;
-      this.flag = flag;
-    
-      return {
-        title,
-        description,
-        dueDate,
-        flag,
-      }
-    });
-  
-    const _getNewNoteData = (function() {
+  notesArr: [],
+  note: (function (project = 'Inbox', title, description, dueDate, flag) {
+    this.project = project;
+    this.title = title;
+    this.description = description;
+    this.dueDate = dueDate;
+    this.flag = flag;
+
+    add: (function() {
+      let _note = _createNote();
+      notesArr.push(_note);
+    }),
+
+    function _getNewNoteData() {
       return {
         title: document.getElementById('title-input').value,
         desc: document.getElementById('desc-input').value,
         date: document.getElementById('date-input').value,
         flag: document.getElementById('flag-btn').checked,
       };
-    });
-    
+    };
+
     function _createNote() {
       let _title = _getNewNoteData().title;
       let _desc = _getNewNoteData().desc;
@@ -36,15 +30,13 @@ const notes = {
       return (new _Note(_title, _desc, _date, _flag));
     };
   
-    function addNote() {
-      let _note = _createNote();
-      notes.push(_note);
-    };
-  
     return {
-      name,
-      notes,
-      addNote,
+      project,
+      title,
+      description,
+      dueDate,
+      flag,
+      add,
     }
   }),
 };
